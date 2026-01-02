7aef857679c14c789794e78e2f6615219f3f6e6e
Gilson Nunes <ndevs.studio@outlook.com>
2026-01-01 23:18:27 -0300

    chore: adds vitest configuration file

---
+++ vitest.config.ts
@@ -0,0 +1,29 @@
+import { loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig(({ mode }) => ({
+	test: {
+		globals: true,
+		environment: "node",
+		clearMocks: true,
+		pool: "threads",
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "json", "html"],
+			reportsDirectory: "./coverage",
+			all: true,
+			include: ["src/**/*.ts"],
+			exclude: ["tests/**/*.ts"],
+		},
+		passWithNoTests: false,
+		env: {
+			CORS_ORIGIN: "http://localhost:3000",
+			...loadEnv(mode, process.cwd(), ""),
+		},
+	},
+	plugins: [tsconfigPaths()],
+	esbuild: {
+		sourcemap: true,
+	},
+}));
