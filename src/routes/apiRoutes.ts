f4d36e26d017998974ec805e50d651cc8467c40d
Gilson Nunes <ndevs.studio@outlook.com>
2026-01-03 11:22:58 -0300

    feat: extracts the basic authentication hook

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,9 +1,5 @@
-import type {
-	FastifyInstance,
-	FastifyReply,
-	FastifyRequest,
-	HookHandlerDoneFunction,
-} from 'fastify';
+import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { basicAuthHook } from '@/hooks/basicAuthHook';
 
 export const apiRoutes = (app: FastifyInstance) => {
 	app.get('/', async (_request: FastifyRequest, reply: FastifyReply) => {
@@ -13,30 +9,7 @@ export const apiRoutes = (app: FastifyInstance) => {
 	app.get(
 		'/protected',
 		{
-			onRequest: (
-				request: FastifyRequest,
-				reply: FastifyReply,
-				done: HookHandlerDoneFunction,
-			) => {
-				const { authorization } = request.headers;
-				if (!authorization || !authorization.startsWith('Basic ')) {
-					reply.status(401).send({ data: 'Access denied' });
-					return;
-				}
-
-				const token = authorization.split(' ')[1];
-				const [username, password] = Buffer.from(token, 'base64')
-					.toString('utf-8')
-					.split(':');
-
-				const notTheUsername = username !== process.env.BASIC_AUTH_USERNAME;
-				const notThePassword = password !== process.env.BASIC_AUTH_PASSWORD;
-				if (notTheUsername || notThePassword) {
-					reply.status(401).send({ data: 'Access denied' });
-					return;
-				}
-				done();
-			},
+			onRequest: basicAuthHook,
 		},
 		async (_request: FastifyRequest, reply: FastifyReply) => {
 			reply.status(200).send({ data: 'Content protected' });
