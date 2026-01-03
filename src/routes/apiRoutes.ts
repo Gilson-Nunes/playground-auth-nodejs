6d9d1717b1564d34f9876e9b6622fdcc933e89e6
Gilson-Nunes <ndevs.studio@outlook.com>
2026-01-03 09:28:27 -0300

    feat: adds protected route
    * feat: adds protected route
    
    * test: adds test for the protected route

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -4,4 +4,15 @@ export const apiRoutes = (app: FastifyInstance) => {
 	app.get('/', async (_request: FastifyRequest, reply: FastifyReply) => {
 		reply.status(200).send({ data: 'Home' });
 	});
+
+	app.get(
+		'/protected',
+		{
+			onRequest: (_request: FastifyRequest, reply: FastifyReply) =>
+				reply.status(401).send({ data: 'Access denied' }),
+		},
+		async (_request: FastifyRequest, reply: FastifyReply) => {
+			reply.status(200).send({ data: 'Content protected' });
+		},
+	);
 };
