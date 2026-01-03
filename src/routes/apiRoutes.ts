b6a0a4c8510f2f4172a7a6f1cd5af06dd878f1d8
Gilson Nunes <ndevs.studio@outlook.com>
2026-01-03 11:15:11 -0300

    feat: adds basic authentication

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,4 +1,9 @@
-import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import type {
+	FastifyInstance,
+	FastifyReply,
+	FastifyRequest,
+	HookHandlerDoneFunction,
+} from 'fastify';
 
 export const apiRoutes = (app: FastifyInstance) => {
 	app.get('/', async (_request: FastifyRequest, reply: FastifyReply) => {
@@ -8,8 +13,30 @@ export const apiRoutes = (app: FastifyInstance) => {
 	app.get(
 		'/protected',
 		{
-			onRequest: (_request: FastifyRequest, reply: FastifyReply) =>
-				reply.status(401).send({ data: 'Access denied' }),
+			onRequest: (
+				request: FastifyRequest,
+				reply: FastifyReply,
+				done: HookHandlerDoneFunction,
+			) => {
+				const { authorization } = request.headers;
+				if (!authorization || !authorization.startsWith('Basic ')) {
+					reply.status(401).send({ data: 'Access denied' });
+					return;
+				}
+
+				const token = authorization.split(' ')[1];
+				const [username, password] = Buffer.from(token, 'base64')
+					.toString('utf-8')
+					.split(':');
+
+				const notTheUsername = username !== process.env.BASIC_AUTH_USERNAME;
+				const notThePassword = password !== process.env.BASIC_AUTH_PASSWORD;
+				if (notTheUsername || notThePassword) {
+					reply.status(401).send({ data: 'Access denied' });
+					return;
+				}
+				done();
+			},
 		},
 		async (_request: FastifyRequest, reply: FastifyReply) => {
 			reply.status(200).send({ data: 'Content protected' });
