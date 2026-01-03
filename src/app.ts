79ee802ddce67601f93e93c07b55c06aba9b830a
Gilson Nunes <ndevs.studio@outlook.com>
2026-01-02 22:35:20 -0300

    feat: adds home route

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,10 @@ import Fastify, { type FastifyServerOptions } from 'fastify';
 
 export function serverBuild(opts: FastifyServerOptions = {}) {
 	const app = Fastify(opts);
+
+	app.get('/', async (_request, reply) => {
+		reply.status(200).send({ data: 'Home' });
+	});
+
 	return app;
 }
