e7812a5f355092d9ee3575742e5fa0eba21e189f
Gilson Nunes <ndevs.studio@outlook.com>
2026-01-04 22:07:48 -0300

    chore: adds database connection with the server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import Fastify, { type FastifyServerOptions } from 'fastify';
+import { getDbPool } from './providers/database';
 import { apiRoutes } from './routes/apiRoutes';
 
 export function serverBuild(opts: FastifyServerOptions = {}) {
 	const app = Fastify(opts);
+
+	app.decorate('db', getDbPool());
 
 	app.register(apiRoutes);
 
