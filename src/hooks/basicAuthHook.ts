4ec8703e4cac075d8eae4f36adcb942be8e21f4e
Gilson-Nunes <ndevs.studio@outlook.com>
2026-01-06 10:30:26 -0300

    feat: adds token unification
    * fix: adds test description correction
    
    * feat: adds the user token to protected routes
    
    * test: adds user token test on protected routes

--- src/hooks/basicAuthHook.ts
+++ src/hooks/basicAuthHook.ts
@@ -1,10 +1,12 @@
+import bcrypt from 'bcrypt';
 import type {
 	FastifyReply,
 	FastifyRequest,
 	HookHandlerDoneFunction,
 } from 'fastify';
+import { getDbPool } from '@/providers/database';
 
-export const basicAuthHook = (
+export const basicAuthHook = async (
 	request: FastifyRequest,
 	reply: FastifyReply,
 	done: HookHandlerDoneFunction,
@@ -20,9 +22,22 @@ export const basicAuthHook = (
 		.toString('utf-8')
 		.split(':');
 
-	const notTheUsername = username !== process.env.BASIC_AUTH_USERNAME;
-	const notThePassword = password !== process.env.BASIC_AUTH_PASSWORD;
-	if (notTheUsername || notThePassword) {
+	const isTheUsername = username === process.env.BASIC_AUTH_USERNAME;
+	const isThePassword = password === process.env.BASIC_AUTH_PASSWORD;
+	if (isTheUsername || isThePassword) {
+		done();
+	}
+
+	const dbPool = getDbPool();
+	const findOneByNameQuery = `SELECT * FROM users WHERE username = $1`;
+	const { rows } = await dbPool.query(findOneByNameQuery, [username]);
+	const user = rows[0];
+	if (!user) {
+		reply.status(401).send({ data: 'Access denied' });
+		return;
+	}
+	const match = await bcrypt.compare(password, user.password);
+	if (!match) {
 		reply.status(401).send({ data: 'Access denied' });
 		return;
 	}
