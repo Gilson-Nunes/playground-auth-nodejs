0c256e075b8aebfd38c980cc53ed1d3cf4524c0b
Gilson-Nunes <ndevs.studio@outlook.com>
2026-01-05 23:17:54 -0300

    feat: adds user login
    * feat: adds user login
    
    * test: adds test for user login

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -32,4 +32,46 @@ export const authRoutes = (app: FastifyInstance) => {
 			}
 		},
 	);
+
+	app.post(
+		'/login',
+		async (
+			request: FastifyRequest<{
+				Body: {
+					username: string;
+					password: string;
+				};
+			}>,
+			reply: FastifyReply,
+		) => {
+			const { username, password } = request.body;
+
+			try {
+				const findOneByNameQuery = `SELECT * FROM users WHERE username = $1`;
+				const { rows } = await app.db.query(findOneByNameQuery, [username]);
+				if (!rows.length) {
+					reply.status(400).send({ data: 'Login error' });
+					return;
+				}
+				const user = rows[0];
+				const match = await bcrypt.compare(password, user.password);
+				if (!match) {
+					reply.status(400).send({ data: 'Login error' });
+					return;
+				}
+				const token = `Basic ${Buffer.from(
+					`${user.username}:${user.password}`,
+					'utf8',
+				).toString('base64')}`;
+				reply.header('Authorization', token);
+				reply.status(200).send({ data: 'User logged' });
+			} catch (error) {
+				if (error instanceof DatabaseError) {
+					reply.status(500).send({ data: 'Internal Server Error' });
+					return;
+				}
+				throw error;
+			}
+		},
+	);
 };
