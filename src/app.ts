fa2b3f020e791f34a1eca5b304dd1f031b5fc8d8
Gilson Nunes <ndevs.studio@outlook.com>
2026-01-06 23:29:36 -0300

    chore: adds @fastify/cookie configuration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cookie from '@fastify/cookie';
 import Fastify, { type FastifyServerOptions } from 'fastify';
 import { getDbPool } from './providers/database';
 import { apiRoutes } from './routes/apiRoutes';
@@ -5,6 +6,10 @@ import { authRoutes } from './routes/authRoutes';
 
 export function serverBuild(opts: FastifyServerOptions = {}) {
 	const app = Fastify(opts);
+
+	app.register(cookie, {
+		secret: process.env.COOKIE_SECRET,
+	});
 
 	app.decorate('db', getDbPool());
 
