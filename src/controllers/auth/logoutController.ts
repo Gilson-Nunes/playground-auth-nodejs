fe319bd9d5d0bc7f351d92cd15fd2b07e7868e93
Gilson-Nunes <ndevs.studio@outlook.com>
2026-01-07 21:00:35 -0300

    Revert "refactor: adds auth controllers"
    This reverts commit f8f744b85c79c5996caf601628a37869ab6f0304.

--- src/controllers/auth/logoutController.ts
+++
@@ -1,9 +0,0 @@
-import type { FastifyReply, FastifyRequest } from 'fastify';
-
-export async function logoutController(
-	_request: FastifyRequest,
-	reply: FastifyReply,
-) {
-	reply.clearCookie('accessToken', { path: '/' });
-	reply.status(204).send();
-}
